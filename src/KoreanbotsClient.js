353e7c648fe023e9a2d79c4739c920c4c86d512b
모메MoMe <alex789189@gmail.com>
2020-08-07 10:18:31 +0900

    Fix(KoreanbotsClient): not Support Djs 9.3.0 lower
    Version 변수가 없는 곳에서는 guild 대신 server로 되어 있으며
    이거까지 지원하기에는 너무 오래된 버전이기에 제한 조치를 하였습니다.

--- src/KoreanbotsClient.js
+++ src/KoreanbotsClient.js
@@ -3,7 +3,7 @@ const DjsVersion = version ? parseInt(version.split(".")[0]) : null
 class KoreanbotsClient extends Client {
     constructor(options = {}) {
         super(options)
-
+        if(!DjsVersion) throw new Error('Can't find Discord.js Version. (It will be >= 9.3.0)');
         this.options.koreanbotsToken = options.koreanbotsToken || null
         this.options.koreanbotsOptions = options.koreanbotsOptions || new Object()
         this.options.koreanbotsOptions.interval = options.koreanbotsOptions.interval || 60000 * 30
@@ -14,12 +14,12 @@ class KoreanbotsClient extends Client {
     }
 
     _update() {
-        const GuildCount = DjsVersion && DjsVersion >= 12 ? this.guilds.cache.size : this.guilds.size
+        const GuildCount = DjsVersion >= 12 ? this.guilds.cache.size : this.guilds.size
         return this.koreanbots.update(GuildCount)
     }
 
     _ok() {
-        const GuildCount = DjsVersion && DjsVersion >= 12 ? this.guilds.cache.size : this.guilds.size
+        const GuildCount = DjsVersion >= 12 ? this.guilds.cache.size : this.guilds.size
         if (!GuildCount) return setTimeout(this._ok, 1000)
 
         const { MyBot } = require("./")
