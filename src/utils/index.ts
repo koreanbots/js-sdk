68b321234edaaf084254d2a4762d645f1cc67dc5
zero734kr <zero734kr@gmail.com>
2021-05-22 22:00:26 -0300

    chore: move cache options validator to Utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,36 @@
 import { promisify } from "util"
 
+import type { ProxyValidator } from "./types"
+
 export function isJSON(content: string): boolean {
     try {
         return !!JSON.parse(content)
-    } catch (e) {
-        if (e instanceof SyntaxError) return false
-
-        throw e
+    } catch {
+        return false
     }
 }
 
 export function waitFor(delay: number): Promise<void> {
     return promisify(setTimeout)(delay)
+}
+
+export function CacheOptionsValidator<T>(): ProxyValidator<T> {
+    return {
+        set(obj, prop, value) {
+            switch (prop) {
+            case "max":
+                if (typeof value !== "number") throw new TypeError(`"max" 옵션은 숫자여야 합니다. (받은 타입: ${typeof value})`)
+                if (value <= 0) throw new RangeError(`"max" 옵션은 0보다 커야 합니다. (받은 값: ${value}, 최소보다 '${1 - value}' 작음)`)
+                if (!Number.isSafeInteger(value)) throw new RangeError(`"max" 옵션은 32비트 정수만 허용됩니다. (받은 값: ${value})`)
+                break
+            case "maxAge":
+                if (typeof value !== "number") throw new TypeError(`"maxAge" 옵션은 숫자여야 합니다. (받은 타입: ${typeof value})`)
+                if (value <= 0) throw new RangeError(`"maxAge" 옵션은 0보다 커야 합니다. (받은 값: ${value}, 최소보다 '${1 - value}' 작음)`)
+                if (!Number.isSafeInteger(value)) throw new RangeError(`"maxAge" 옵션은 32비트 정수만 허용됩니다. (받은 값: ${value})`)
+            }
+
+            obj[prop] = value
+            return true
+        }
+    }
 }
