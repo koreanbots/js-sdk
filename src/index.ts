896ef34e6f100fd0e363a6c83621125dfe6e97c8
zero734kr <zero734kr@gmail.com>
2021-05-22 22:00:40 -0300

    chore: export managers

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@
  * @type {import("./client/Koreanbots")}
  */
 export { Koreanbots } from "./client/Koreanbots"
+
+export { BotManager } from "./managers/BotManager"
+export { UserManager } from "./managers/UserManager"
+export { WidgetManager } from "./managers/WidgetManager"
