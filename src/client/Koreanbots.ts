141295e95af695a1420c862b0ed1d0685e1dc452
zero734kr <zero734kr@gmail.com>
2021-05-24 07:17:50 -0300

    Update src/client/Koreanbots.ts
    Co-authored-by: Junseo Park <wonderbotsupport@naver.com>

--- src/client/Koreanbots.ts
+++ src/client/Koreanbots.ts
@@ -23,7 +23,7 @@ export class Koreanbots {
             switch (prop) {
             case "clientID":
                 if (typeof value !== "string") throw new TypeError(`"clientID" 옵션은 문자열이여야 합니다. (받은 타입: ${typeof value})`)
-                if (!snowflakeRegex.test(obj[prop] as unknown as string)) throw new TypeError("\"clientID\" 옵션은 디스코드의 ID 체계인 Snowflake여야 합니다.")
+                if (!snowflakeRegex.test(obj[prop] as unknown as string)) throw new TypeError("\"clientID\" 옵션은 Snowflake여야 합니다.")
                 break
             }
 
