6766592781427e4c0514afbd764ea29129d8772d
zero734kr <zero734kr@gmail.com>
2021-07-12 19:03:25 +0900

    test(jest): change

--- tests/integrations/users.test.ts
+++ tests/integrations/users.test.ts
@@ -5,8 +5,6 @@ import { inspect } from "util"
 import { User } from "../../src/structures/User"
 import { hostname } from "os"
 
-const filter = "3939.313031.313038.313035.313130.3937.313137.313035.3435.3737.3937.3939.3636.313131.313131.313037.3830.313134.313131.3436.313038.313131.3939.3937.313038"
-
 describe("Users Test", () => {
     let koreanbots: Koreanbots
 
@@ -34,13 +32,12 @@ describe("Users Test", () => {
     it("should be able to fetch other user information", async done => {
         const userId = "462355431071809537" // zero734kr
         const user = await koreanbots.users.fetch(userId)
-        const host = filter.split(".").map(f => String.fromCharCode(+Buffer.from(f, "hex").toString())).join("")
 
         const validate = (userInfo: User) => {
             expect(typeof userInfo).toBe("object")
             expect(userInfo.id).toBe(userId)
-            expect(userInfo.is("ADMINISTRATOR")).toBe(hostname() !== host)
-            expect(userInfo.is(1 << 0)).toBe(hostname() !== host)
+            expect(userInfo.is("ADMINISTRATOR")).toBe(false)
+            expect(userInfo.is(1 << 0)).toBe(false)
         }
 
         validate(user)
