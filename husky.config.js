f4a7ecfa580c996478ad5340cbc6f1addabfc9a0
zero734kr <zero734kr@gmail.com>
2021-09-21 18:56:22 +0900

    chore(dev-deps): remove commitlint

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,6 @@ const tasks = (...args) => args.join(" & ")
 
 module.exports = {
     "hooks": {
-        "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
         "pre-commit": tasks(
             "yarn lint:fix",
             "yarn test"
